ed2fed57945e3e65bb1266e0afdd46f0ed9b6383
A-Kolesnikov <kolesnikov.spb@gmail.com>
2023-08-19 11:41:52 +0300

    installed necessary packages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
 import React from 'react'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container } from 'react-bootstrap'
+
+import { Routes, Route } from 'react-router-dom'
+
 import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
-    <div>
+    <Container fluid>
       <h1>Welcome to GPS-store!</h1>
-      <HomePage/>
-    </div>
+      <Routes>
+        <Route path = '/' element = {<HomePage />} />
+        <Route path = '/login' element = {<LoginPage />} />
+      </Routes>
+    </Container>
   );
 }
 
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,9 +5,9 @@ import axios from "axios";
 import ProductCardVertical from "./components/ProductCardVertical";
 
 function HomePage(){
-    const data = axios.get('http://localhost:3100/products')
-    data.then(res => {
-        console.log(res.data)
+    const fetchedData = axios.get('http://localhost:3100/products')
+    fetchedData.then(res => {
+        console.log(res)
     })
     const [products, setProducts] = useState([])
     return(
