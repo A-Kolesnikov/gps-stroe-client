43a450199c4d401391974ae92bc8875795aa48f3
A-Kolesnikov <kolesnikov.spb@gmail.com>
2023-09-18 12:33:22 +0300

    implemented adding to cart and inCart indication for productCards

--- src/pages/hooks/contexts/userContext.js
+++ src/pages/hooks/contexts/userContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from "react"
+import React, { createContext, useState, useEffect } from "react"
 
 import useFetch from "../useFetch"
 import useCurrentUser from "../useCurrentUser"
@@ -10,34 +10,31 @@ const serverUrl = process.env.REACT_APP_SERVER_URL
 
 export default function UserContextProvider({ children }) {
 
-    const defaultCartSettings = {action: "show", product_id: null, quantity: 1}
-    let cartSettings = defaultCartSettings
-
     //User details
     const [userTrigger, setUserTrigger] = useState(false)
     const handleUserTrigger = () => {
-        cartSettings = defaultCartSettings
         setUserTrigger(prevStatus => !prevStatus)
     }
     const { currentUser, visitCounter, currentWishList, logout } = useCurrentUser(userTrigger, handleUserTrigger)
 
     //Cart details
-    
-    /*const [cartMode, setCartMode] = useState({url: !currentUser ? null : `/by-user/${currentUser.id}`, method:'GET', requestBody: null})
-    useEffect(()=>{
-
-    }, [currentUser])
-
-    const finalUrl = !currentUser ? null : `${serverUrl}${cartMode.url}`
-    const { data: currentCart, error: cartError, loading: cartLoading } = useFetch(finalUrl, cartMode.method, cartMode.requestBody)
-    console.log(finalUrl, currentCart)*/
 
     const [cartTrigger, setCartTrigger] = useState(false)
-    const  { data: currentCart } = useCart(cartTrigger, currentUser, cartSettings.action, cartSettings.product_id, cartSettings.quantity)
+    const [cartSettings, setCartSettings] = useState({action: "show", product_id: null, quantity: 1})
+
+    const  { data: currentCart, error: cartError } = useCart(cartTrigger, currentUser, cartSettings.action, cartSettings.product_id, cartSettings.quantity)
+    if(cartError){
+        console.error(cartError)
+    }
+
     const handleCartTrigger = (action = "show", product_id = null, quantity = 1) => {
-        cartSettings.action = action
-        cartSettings.product_id = product_id
-        cartSettings.quantity = quantity
+        const newCartSettings = {
+            ...cartSettings,
+            action: action,
+            product_id: product_id,
+            quantity: quantity,
+        }
+        setCartSettings(newCartSettings)
         setCartTrigger(prevStatus => !prevStatus)
     }
     console.log(currentCart)
