8197dd94ed17fb671985154d3b2ce51a3b4680b0
A-Kolesnikov <kolesnikov.spb@gmail.com>
2023-09-18 10:03:47 +0300

    implemented useCart hook in userContext

--- src/pages/hooks/contexts/userContext.js
+++ src/pages/hooks/contexts/userContext.js
@@ -2,6 +2,7 @@ import React, { createContext, useState } from "react"
 
 import useFetch from "../useFetch"
 import useCurrentUser from "../useCurrentUser"
+import useCart from "../useCart"
 
 export const UserContext = createContext(null)
 
@@ -9,14 +10,19 @@ const serverUrl = process.env.REACT_APP_SERVER_URL
 
 export default function UserContextProvider({ children }) {
 
+    const defaultCartSettings = {action: "show", product_id: null, quantity: 1}
+    let cartSettings = defaultCartSettings
+
     //User details
     const [userTrigger, setUserTrigger] = useState(false)
     const handleUserTrigger = () => {
+        cartSettings = defaultCartSettings
         setUserTrigger(prevStatus => !prevStatus)
     }
-    const { currentUser, visitCounter, currentWishList, currentCart, logout } = useCurrentUser(userTrigger, handleUserTrigger)
+    const { currentUser, visitCounter, currentWishList, logout } = useCurrentUser(userTrigger, handleUserTrigger)
 
     //Cart details
+    
     /*const [cartMode, setCartMode] = useState({url: !currentUser ? null : `/by-user/${currentUser.id}`, method:'GET', requestBody: null})
     useEffect(()=>{
 
@@ -26,8 +32,18 @@ export default function UserContextProvider({ children }) {
     const { data: currentCart, error: cartError, loading: cartLoading } = useFetch(finalUrl, cartMode.method, cartMode.requestBody)
     console.log(finalUrl, currentCart)*/
 
+    const [cartTrigger, setCartTrigger] = useState(false)
+    const  { data: currentCart } = useCart(cartTrigger, currentUser, cartSettings.action, cartSettings.product_id, cartSettings.quantity)
+    const handleCartTrigger = (action = "show", product_id = null, quantity = 1) => {
+        cartSettings.action = action
+        cartSettings.product_id = product_id
+        cartSettings.quantity = quantity
+        setCartTrigger(prevStatus => !prevStatus)
+    }
+    console.log(currentCart)
+
     return (
-        <UserContext.Provider value={{ currentUser, visitCounter, currentCart, currentWishList, logout, handleUserTrigger }}>
+        <UserContext.Provider value={{ currentUser, visitCounter, currentCart, currentWishList, logout, handleUserTrigger, handleCartTrigger }}>
             {children}
         </UserContext.Provider>
     )
