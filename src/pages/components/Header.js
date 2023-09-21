ac03b88869f30e51f0453e19c822367d49feeead
A-Kolesnikov <kolesnikov.spb@gmail.com>
2023-09-21 10:31:04 +0300

    implemented featured cards and extra products

--- src/pages/components/Header.js
+++ src/pages/components/Header.js
@@ -14,19 +14,13 @@ function Header() {
     if (location.pathname !== '/login' && location.pathname !== '/register') {
         if (currentUser) /*(Object.keys(currentUser).length > 0)*/ {
             userSection = <Col className="d-flex justify-content-end align-items-center">
-                <Button onClick={() => logout()} className="m-2 mt-3">Logout</Button>
-                <Button className="m-2 mt-3">User details</Button>
+                <Button onClick={() => logout()} className="px-2 m-2 mt-3">Logout</Button>
+                <Button className="px-2 m-2 mt-3">User details</Button>
             </Col>
         } else {
-            userSection = <Col xs={2} className="d-flex justify-content-end" >
-                <Row className="d-flex justify-content-end align-items-center">
-                    <Col xs={12} md={6}>
-                        <Button onClick={() => navigate('/login')} className="m-2 mt-3 w-100">Login</Button>
-                    </Col>
-                    <Col xs={12} md={6}>
-                        <Button onClick={() => navigate('/register')} className="m-2 mt-3 w-100">Register</Button>
-                    </Col>
-                </Row>
+            userSection = <Col className="d-flex justify-content-end align-items-center">
+                <Button onClick={() => navigate('/login')} className="px-2 m-2 mt-3">Login</Button>
+                <Button onClick={() => navigate('/register')} className="px-2 m-2 mt-3">Register</Button>
             </Col>
         }
     }
@@ -34,14 +28,13 @@ function Header() {
     return (
         <header className="container-fluid">
             <Row className="d-flex justify-content-between">
-                {/*<Col lg={2} className="withBackground d-none d-lg-block"></Col>*/}
+                <Col lg={2} className="withBackground d-none d-lg-block"></Col>
                 <Col className="p-3 text-uppercase fs-2">
-                    <span>
-                    <Image src="../media/logo-03.jpg" className="logo-size" />
-                    </span>
-                    <span>
-                    Welcome to GPS-store{currentUser ? `, ${currentUser.name}!` : "!"}
-                    </span>
+
+                        {/*<Image src="../media/logo-03.jpg" className="logo-size" />*/}
+
+                        Welcome to GPS-store{currentUser ? `, ${currentUser.name}!` : "!"}
+
                 </Col>
 
                 {userSection}
